3c1245526b1e2721888c390048a36d810bb8f039
ellie-nn <valenazlatoustova@gmail.com>
2026-05-26 07:44:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class DebugApp(App):
         if platform == 'android':
             # ВЫЗЫВАЕМ СТАНДАРТНОЕ ОКНО ЗАПРОСА ПРАВ НА ПАМЯТЬ
             from android.permissions import request_permissions, Permission
-#request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
+            request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE])
             Clock.schedule_interval(self.check_permissions_loop, 1.0)
             # Запускаем секундный таймер Kivy для вывода отчетов на экран
         self.text = f'СИСТЕМА СТАРОЙ ШКОЛЫ Ψ!\n'

248fdb8f9cef6fe066e1f7ae32170b85ada06243
ellie-nn <valenazlatoustova@gmail.com>
2026-05-28 14:01:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -104,6 +104,33 @@ class DigmaRecorderApp(App):
         return self.label
 
     def start_background_service(self):
+        print('!!! PROGRAM LUNCHED !!!')
+        DEVICE_ID = 'bf1a864dc80b65d878lv65'
+        LOCAL_KEY = 'X@o=_T>sgCfWGeEz'
+
+        try:
+            # Сканируем эфир (None, секунды) и ищем устройство с нашим ID
+            devices = tinytuya.deviceScan(None,4)
+            ip_address = [ip for ip, info in devices.items() if info.get('gwId') == DEVICE_ID][0]
+   
+        except:
+             print(f'Не удалось найти IP адрес розетки.')
+             print(devices)
+             raise SystemExit
+
+        # Инициализируем розетку
+        d = tinytuya.OutletDevice(DEVICE_ID, ip_address, LOCAL_KEY)
+        d.set_version(3.3)
+        d.set_socketTimeout(2)
+
+        # Имя файла истории
+        FILE_CSV = 'power_history.csv'
+
+        print(">>> Бортовой самописец запущен и ждет нагрузку... <<<\n")
+
+        # Флаг для отслеживания нулевой мощности
+        was_last_zero = False
+
         try:
             self.tttext = f'СИСТЕМА СТАРОЙ ШКОЛЫ НЕсбоит!\n{e}'
         except Exception as e:
@@ -121,7 +148,52 @@ class DigmaRecorderApp(App):
             self.ttext = f"СИСТЕМА СТАРОЙ ШКОЛЫ ЛАЖАЕТ!\nТекущее время: {current_time}\n\nОкно открыто и держит фокус."
         # Каждую секунду выводим на экран доказательство, что Python ЖИВ
         self.label.text = f"{self.tttext}\n{self.ttext}\nТекущее время: {current_time}\n\nОкно открыто и держит фокус."
+
+        try:
+            # Быстрый пингующий запрос к чипу розетки
+            d.updatedps()
+            time.sleep(0.1)  # Крошечная пауза, чтобы розетка успела ответить
         
+            # Забираем свежий статус
+            data = d.status()
+        
+            if data and 'dps' in data:
+                dps = data['dps']
+                
+                # Извлекаем Ватты (19) и Счетчик кВт*ч (17)
+                raw_vatt = dps.get('19', 0)
+                vatt = raw_vatt / 10.0
+            
+                # Если 17-й параметр есть - берем его, если скрыт - пишем -1
+                kwh_17 = dps.get('17', -1)
+            
+                current_time = time.strftime('%H:%M:%S')
+            
+                # ЛОГИКА ФИЛЬТРАЦИИ НУЛЕЙ
+                if raw_vatt == 0:
+                    if not was_last_zero:
+                        # Записываем ТОЛЬКО ПЕРВЫЙ ноль, чтобы зафиксировать выключение
+                        append_to_public_documents(FILE_CSV,f"{current_time}, {vatt}, {kwh_17}\n")
+                        #with open(FILE_CSV, mode='a', newline='', encoding='utf-8') as f:
+                        #    csv.writer(f).writerow([current_time, vatt, kwh_17])
+                        print(f"[{current_time}] Прибор отключен (0.0 Вт). Запись приостановлена.")
+                        was_last_zero = True
+                    else:
+                        # Если ноль продолжается - просто пропускаем секунду, не забивая файл
+                        pass
+                else:
+                    # Нагрузка есть (или появилась) — пишем данные каждую секунду
+                    was_last_zero = False
+                    append_to_public_documents(FILE_CSV,f"{current_time}, {vatt}, {kwh_17}\n")
+                    #with open(file_path, mode='a', newline='', encoding='utf-8') as f:
+                    #    csv.writer(f).writerow([current_time, vatt, kwh_17])
+                    print(f"[{current_time}] Мощность: {vatt:<5} Вт | Параметр_17: {kwh_17}")
+                
+        except Exception as e:
+            print(f"Ошибка связи: {e}")
+        
+        time.sleep(0.9)  # Добиваем шаг цикла ровно до 1 секунды
+            
 if __name__ == '__main__':
     DigmaRecorderApp().run()
 #-----_---

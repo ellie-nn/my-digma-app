649dedaca6bb51aaafb0ddec783b935a5121eac6
ellie-nn <valenazlatoustova@gmail.com>
2026-05-31 06:13:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -113,6 +113,21 @@ class MediaStoreStdout:
 # ИМПОРТИРУЕМ ДАТЧИК ОКНА
 class DigmaRecorderApp(App):
     def build(self):
+        # === ТЕСТОВЫЙ ВИБРО-ПИНОК СТАРТА СЛУЖБЫ ===
+        try:
+    # 1. Достаем контекст живой фоновой службы Kivy
+            Context = autoclass('org.kivy.android.PythonService').mService
+    
+    # 2. Вызываем официальную системную службу вибрации Android
+            vibrator = Context.getSystemService(Context.VIBRATOR_SERVICE)
+    
+    # 3. Трясем телефон 2000 миллисекунд (2 секунды)
+            vibrator.vibrate(2000)
+        except Exception as vib_err:
+    # Если мы упали на старте — этот принт улетит в системный Logcat
+            print(f"Ошибка вибромотора: {vib_err}")
+# ==========================================
+        
         
         try:
             # мост к Java-службам Android

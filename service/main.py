de214361971cdf5e3d7ae0793bb03b2f4eb0e2c9
ellie-nn <valenazlatoustova@gmail.com>
2026-06-01 03:43:11 +0300

    Update main.py

--- service/main.py
+++ service/main.py
@@ -118,6 +118,7 @@ class DigmaServiceEngine:
         time.sleep(2.0)
         
         print('stdouttestend')
+        self.vatt_sum = 0
         while True:
             append_to_public_documents(FDATA_NAME, 'loop')
             self.update_data()

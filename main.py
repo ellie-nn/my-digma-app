a5726f1eaa2765be28739db55f2eb027a467f47b
ellie-nn <valenazlatoustova@gmail.com>
2026-06-14 00:30:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ class MediaStoreStdout:
         # Если прилетает не пустая строка — отправляем её в наш Java-мост
         if message and message.strip():
             # Вызываем вашу отлаженную функцию дозаписи в Documents!
-            #append_to_public_documents(LOG_FN+".txt", message.strip())
+            append_to_public_documents(LOG_FN+".txt", message.strip())
             #time.sleep(1.0)
             apd(LOG_FN+"x.txt", message.strip())
     def flush(self):

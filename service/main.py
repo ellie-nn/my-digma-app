448e624a1d1c7c977cd6575307851319bafe8425
ellie-nn <valenazlatoustova@gmail.com>
2026-06-15 14:40:40 +0300

    Update main.py

--- service/main.py
+++ service/main.py
@@ -23,7 +23,8 @@ FDATA_NAME = "servicework.txt"
 FSVC_LOG = "srv_log.txt"
 DEVICE_ID = "bf1a864dc80b65d878lv65"
 LOCAL_KEY = "X@o=_T>sgCfWGeEz"
-SUB_DIR = "digma/" if os.android.get('ANDROID_ARGUMENT','')=='digmarecorderok' else ''
+#SUB_DIR = "digma/" if os.android.get('ANDROID_ARGUMENT','')=='digmarecorderok' else ''
+SUB_DIR=""
 def append_to_public_documents(filename, text_content):
     try:
         # ХИРУРГИЧЕСКИЙ ФИКС ДЛЯ СЛУЖБЫ:

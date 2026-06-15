99299c02652bf745a5357ccd19b29754a5e3e1b8
ellie-nn <valenazlatoustova@gmail.com>
2026-06-15 03:24:48 +0300

    Update main.py

--- service/main.py
+++ service/main.py
@@ -161,9 +161,9 @@ class DigmaServiceEngine:
             self.last_time = time_
                 
             printout = f"{self.count} {time.strftime('%H:%M:%S')} {vatt} {self.vatt_sum/3600:.3f} {kwh_17}"
-            append_to_public_documents(FDATA_NAME,printout)
         else:
             printout = f"{self.count} {time.strftime('%H:%M:%S')} -1 -1 -1"
+        append_to_public_documents(FDATA_NAME,printout)
         
         self.tttext = printout
         # Каждую секунду выводим на экран доказательство, что Python ЖИВ

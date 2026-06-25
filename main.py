3e8d8643e88f08669c95902828dfe5eaece9f856
ellie-nn <valenazlatoustova@gmail.com>
2026-06-25 08:06:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -819,7 +819,7 @@ class DigmaRecorderApp(App):
         if not self.datafn:
             self.datafn=f'data{int(tstamp)}.txt'
         f=open(f'/storage/emulated/0/Documents/'+self.datafn,'a', encoding="utf-8", errors="ignore")
-        if self.kilometers:
+        if False:#self.kilometers:
             f.write(f'-{self.kilometers} -\n')   
             g=open(f'/storage/emulated/0/Documents/ini.txt','a', encoding="utf-8", errors="ignore")
             g.write(f'self.kilometers = {self.kilometers}\n')

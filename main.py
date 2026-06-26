571fdf3ec63a89d813c9fe2a462aaa2db36e6f76
ellie-nn <valenazlatoustova@gmail.com>
2026-06-26 12:14:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -361,7 +361,7 @@ if True:
             
         graph = Graph(
             xlabel='Время', ylabel='Вольты',
-            x_ticks_minor=10, x_ticks_major=60,
+            x_ticks_minor=6, x_ticks_major=60,
             y_ticks_minor=5, y_ticks_major=10,
             y_grid_label=True, x_grid_label=True,
             padding=10, x_grid=True, y_grid=True,

8da3f58f7a03cbfe9163af971b36a2ed059954dc
ellie-nn <valenazlatoustova@gmail.com>
2026-06-26 09:58:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -440,7 +440,7 @@ if True:
     def move_window(instance, value):
         # Допустим, ширина видимого окна графика на экране — всегда 60 секунд
         #with instance.gw as q:
-        instance.gw.xmin = int(value - (instance.gw.xmax-instance.gw.xmin))
+        instance.gw.xmin = (value - (instance.gw.xmax-instance.gw.xmin))
         instance.gw.xmax = value 
         return
             

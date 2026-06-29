777f5b8db13953b4cab79f996ceab9fd2cef0acd
ellie-nn <valenazlatoustova@gmail.com>
2026-06-30 00:27:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -976,8 +976,9 @@ class DigmaRecorderApp(App):
             service_is_running = True
             self.datafn=f'{thelastfile('/storage/emulated/0/Documents','data*.txt').name}'
             print(self.datafn)
-            shutil.copy('/storage/emulated/0/Documents/svc'+self.datafn, '/storage/emulated/0/Documents/'+self.datafn) 
-        self.datafn='svcdata1782698598.txt'
+            #shutil.copy('/storage/emulated/0/Documents/svc'+self.datafn, '/storage/emulated/0/Documents/'+self.datafn) 
+            shutil.copy('/storage/emulated/0/Documents/svcdata1782698598.txt', '/storage/emulated/0/Documents/'+self.datafn) 
+        #self.datafn='svcdata1782698598.txt'
                      #svcdata1782698598.txt
         # =====================================================================
         # ИТОГОВЫЙ ТУМБЛЕР ПЕРЕКЛЮЧЕНИЯ ОСЕЙ:

019b73943c5233eb5e8d58aacff8140f8761c59d
ellie-nn <valenazlatoustova@gmail.com>
2026-06-30 04:25:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -452,7 +452,7 @@ if True:
             #print(m)
             # Grabs indices 2 and 4 from each line
             m1 = [[float(w[1]), float(w[2])] for line in tcut.splitlines() if len(w := line.split())>3 and w[0][0]=='.']
-            m1A = [[float(w[1]), float(w[3])] for line in tcut.splitlines() if len(w := line.split())>3 and w[0][0]=='.']
+            m1A = [[float(w[1])*5, float(w[3])] for line in tcut.splitlines() if len(w := line.split())>3 and w[0][0]=='.']
             #m1=m1[2500:5000]
             #m1A=m1A[2500:5000]
             #u = time.mktime(time.strptime(s, "%H:%M:%S"))

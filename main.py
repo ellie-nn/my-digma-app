1b721f7f4d95c04bf98f70c74ede7fc9ddc54ea1
ellie-nn <valenazlatoustova@gmail.com>
2026-07-03 00:46:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1030,7 +1030,7 @@ class DigmaRecorderApp(App):
             # библиотека oscpy выкинет официальный крэш RuntimeError (Address already in use)! [↑]
             # Наш блок except ловит этот сигнал и выдает зрячий вердикт: мотор жив! [↑]
             service_is_running = True
-            self.datafn=f'{thelastfile(LOG_PATH[:-1],'data*.txt').name}'
+            self.datafn=f'{thelastfile(LOG_PATH,'data*.txt').name}'
             print(self.datafn)
             shutil.copy(LOG_PATH+'svc'+self.datafn, LOG_PATH+self.datafn) 
             #shutil.copy('/storage/emulated/0/Documents/svcdata1782698598.txt', '/storage/emulated/0/Documents/'+self.datafn) 

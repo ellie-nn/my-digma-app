42d4d7bbf7791df5e63486fa53037398fb1d4c66
ellie-nn <valenazlatoustova@gmail.com>
2026-07-04 20:02:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -743,7 +743,7 @@ def question(main_layout):
         )
         # Привязываем кнопку к нашей будущей функции очистки файла [↑]
     btn_test.bind(on_release=test_btn)
-    main_layout.add_widge(btn_test)
+    main_layout.add_widget(btn_test)
       
     return
         

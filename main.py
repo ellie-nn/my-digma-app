22d838908d0f998d872c179c07eec5dd7bcbe5d8
ellie-nn <valenazlatoustova@gmail.com>
2026-07-05 00:50:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -764,8 +764,9 @@ def g_init(mainclass):
         # Занимает 100% ширины и 100% высоты окна [↑]
         graph_widget.size_hint = (1.0, 0.90) 
         graph_widget.pos_hint = {'center_x': 0.5, 'y':0.05}
-        main_layout.add_widget(graph_widget)
         return main_layout
+        main_layout.add_widget(graph_widget)
+        
         main_layout.graph_widget=graph_widget
         graph_widget.parent=main_layout
         from kivy.uix.label import Label

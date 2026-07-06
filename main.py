0daac0acd9e95d868487c7b792f71151c8aa5bd2
ellie-nn <valenazlatoustova@gmail.com>
2026-07-06 23:51:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -871,7 +871,7 @@ def g_init(mainclass):
         #main_layout.add_widget(log_screen) 
 
         print('869 mainclass.tmax,xmax-xmin',mainclass.tmax,graph_widget.xmax-graph_widget.xmin)
-        scroll_bar_scale = Slider(min=scale_revfunc(10), max=scale_revfunc(mainclass.tmax), value=scale_revfunc(graph_widget.xmax-graph_widget.xmin), orientation='horizontal')
+        scroll_bar_scale = Slider(min=scale_revfunc(10), max=scale_revfunc(max(mainclass.tmax,graph_widget.xmax), value=scale_revfunc(graph_widget.xmax-graph_widget.xmin), orientation='horizontal')
         scroll_bar_scale.gw = graph_widget
         scroll_bar_scale.bind(value=scale_window)
 

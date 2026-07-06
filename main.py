28bee0029a7bfabe25713f656de6ce8f9fc90bdb
ellie-nn <valenazlatoustova@gmail.com>
2026-07-06 18:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1135,7 +1135,7 @@ class DigmaRecorderApp(App):
                 #shutil.copy('/storage/emulated/0/Documents/svcdata1782698598.txt', '/storage/emulated/0/Documents/'+self.datafn) 
             #self.datafn='svcdata1782698598.txt'
                          #svcdata1782698598.txt
-            
+            if MODE=="Тест"
             #Тестовая имитация ранее запущенного сервис-мотора
             self.datafn="AppDataTest.txt"
             service_is_running = True

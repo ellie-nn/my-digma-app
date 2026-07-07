2b65ec2d849bf98b9f32d863f5842ee860093406
ellie-nn <valenazlatoustova@gmail.com>
2026-07-07 16:26:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -481,6 +481,8 @@ def update_points_from_file(graph,fn):
             #mainclass.datafn=f'data_{int(m1[0][1])}.txt'
             for x in reversed(m1): x[0]+=-m1[0][0]+m[-1][0]+1
             for x in reversed(m1A): x[0]+=-m1A[0][0]+m[-1][0]+1
+            m=[[0.0,0.0]]
+            mA=m
             mA=mA+m1A
             m=m+m1
             #for x in m: x[0]/=60

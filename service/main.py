bf30a58f13b131eea04e503c45c3a9ff67787f83
ellie-nn <valenazlatoustova@gmail.com>
2026-07-07 23:15:16 +0300

    Update main.py

--- service/main.py
+++ service/main.py
@@ -25,7 +25,8 @@ DEVICE_ID = "bf1a864dc80b65d878lv65"
 LOCAL_KEY = "X@o=_T>sgCfWGeEz"
 #SUB_DIR = "digma/" if os.android.get('ANDROID_ARGUMENT','')=='digmarecorderok' else ''
 SUB_DIR=''
-SUB_TIME = os.path.getmtime(__file__) # Узнаем точное время создания/изменения нашего файла
+#SUB_TIME = os.path.getmtime(__file__) # Узнаем точное время создания/изменения нашего файла
+SUB_TIME= float(os.environ.get('PYTHON_SERVICE_ARGUMENT', '0.0'))
 #FDATA_NAME = "servicework1.txt" +str(time.time()//60)+".txt"
 #FDATA_NAME = f"service_work_{int(SUB_TIME)}.txt"
 FDATA_NAME = '' 

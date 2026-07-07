095878b94a916a0911c8ed7b3cf3f6a2c8b19d66
ellie-nn <valenazlatoustova@gmail.com>
2026-07-07 13:42:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -530,7 +530,8 @@ if True:
             for i in range(0,step-1): xm1.append(m1[i::step]) 
             m1 = [[sum(values)/step for values in zip(*matrix)] for matrix in zip(*xm1)]
             #m1=result
-
+            print('533 m1:', m1)
+            
             xm1A=[]
             for i in range(0,step-1): xm1A.append(m1A[i::step]) 
             m1A = [[sum(values)/step/5 for values in zip(*matrix)] for matrix in zip(*xm1A)]

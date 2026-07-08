6263b44b25ec4dea49b2db904136ebe3fd17fa86
ellie-nn <valenazlatoustova@gmail.com>
2026-07-08 23:30:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1419,7 +1419,7 @@ class DigmaRecorderApp(App):
         return self.mywin
         
     def display_live_data(self,count,tstamp, vatt, integral,kwh):
-        print('1374 self.datafn',self.datafn)
+        #print('1374 self.datafn',self.datafn)
         if not self.datafn: return
         print('1424 instructions:',count_instructions(Window))
         #if self.histtmax>1000: return

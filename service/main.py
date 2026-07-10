ebcfda2919c92d70cbb2406e953380906de6fbb4
ellie-nn <valenazlatoustova@gmail.com>
2026-07-11 02:48:16 +0300

    Update main.py

--- service/main.py
+++ service/main.py
@@ -317,6 +317,8 @@ class DigmaServiceEngine:
         append_to_public_documents(FDATA_NAME,printout)
         print('Fdatanamef')
         print(int(os.environ.get('PYTHON_SERVICE_ARGUMENT', '0.0').split('_')[0]))
+        print(int(os.environ.get('PYTHON_SERVICE_ARGUMENT', '0.0').split('_')[1]))
+        print(int(os.environ.get('PYTHON_SERVICE_ARGUMENT', '0.0').split('_')[2]))
        
         try:
             # Стреляем пакетом по внутреннему адресу телефона (127.0.0.1) на порт 3000

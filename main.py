2b63bf045b146ed7da343b32fabc7bf5097a648b
ellie-nn <valenazlatoustova@gmail.com>
2026-07-12 21:17:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1234,7 +1234,7 @@ def g_init(mainclass):
         btn_refresh_data.bind(on_release = btn_refresh_data_f)
         main_layout.add_widget(btn_refresh_data)
         
-        btn_StopSave_cr(mainclass)
+        btn_StopSave_cr(main_layout)
         
     return main_layout
           

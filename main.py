79d216dbe3f2da22ffdc1571a5a2da209e8e9191
ellie-nn <valenazlatoustova@gmail.com>
2026-07-12 18:31:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ vibrator.vibrate(500)
 time.sleep(1.0)
 
 # СТРОИМ КЛАСС-ПЕРЕХВАТЧИК
-LOG_PATH=os.path.realpath(os.environ.get('EXTERNAL_STORAGE'))+'/Documents/'
+LOG_PATH=os.path.realpath(os.environ.get('EXTERNAL_STORAGE'))+'/Download/'
 print(LOG_PATH)
 class MediaStoreStdout:
     def __init__(self, outf = 'app_log.txt'):
